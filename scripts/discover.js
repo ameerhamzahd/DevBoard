3e18c0cffc96cc0936a7ee9457153a0cc4d8e3fb
Ameer Hamzah Daiyan <ameerhamzah.daiyan@gmail.com>
2025-03-02 01:18:55 +0600

    DevBoard discover-functionality done.

---
+++ scripts/discover.js
@@ -0,0 +1,7 @@
+const discoverBtn = document.getElementById('discover-btn');
+
+discoverBtn.addEventListener('click', (event) => {
+    if(discoverBtn) {
+        window.location.href = 'blog.html';
+    }
+});
